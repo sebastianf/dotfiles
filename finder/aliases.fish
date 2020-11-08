0905f76fa34f4ddf6074c4ebae211ba23a4bdd17
Sebastian Friedrich <sebastian.friedrich@intive.com>
2020-11-08 12:26:44 +0100

    add finder aliases

---
+++ finder/aliases.fish
@@ -0,0 +1,3 @@
+alias fixow='/System/Library/Frameworks/CoreServices.framework/Versions/A/Frameworks/LaunchServices.framework/Versions/A/Support/lsregister -kill -r -domain local -domain user;killall Finder;echo "Open With has been rebuilt, Finder will relaunch"'
+alias showHiddenFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+alias hideHiddenFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
