68359c5b51e6f04b1df246c48a9e0797bcb6a3ff
Sebastian Friedrich <sebastian.friedrich@intive.com>
2020-11-08 11:11:45 +0100

    change java path

--- java/path.fish
+++ java/path.fish
@@ -2,7 +2,7 @@
 
 set -gx GROOVY_HOME /usr/local/opt/groovy/libexec
 
-set -gx JAVA_11_HOME (/usr/libexec/java_home -v11)
+# set -gx JAVA_11_HOME (/usr/libexec/java_home -v11)
 set -gx JAVA_14_HOME (/usr/libexec/java_home -v14)
 
-set -gx JAVA_HOME $JAVA_11_HOME
+set -gx JAVA_HOME $JAVA_14_HOME
