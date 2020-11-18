de75a3d96f067350121b2238f858df92532669db
Sebastian Friedrich <sebastian.friedrich@mac.com>
2020-11-18 23:07:39 +0100

    updates

--- java/path.fish
+++ java/path.fish
@@ -3,7 +3,7 @@
 set -gx GROOVY_HOME /usr/local/opt/groovy/libexec
 
 # https://stackoverflow.com/questions/52524112/how-do-i-install-java-on-mac-osx-allowing-version-switching/52524114#52524114
-# set -gx JAVA_11_HOME (/usr/libexec/java_home -v11)
+set -gx JAVA_11_HOME (/usr/libexec/java_home -v 1.8)
 set -gx JAVA_14_HOME (/usr/libexec/java_home -v14)
 
-set -gx JAVA_HOME $JAVA_14_HOME
+set -gx JAVA_HOME $JAVA_11_HOME
