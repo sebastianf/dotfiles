18851281e5925e82ff614af0cdd9a0d121494089
Sebastian Friedrich <sebastian.friedrich@mac.com>
2020-11-30 14:22:20 +0100

    add neofetch - things cli - tldr

--- fish/window.fish
+++ fish/window.fish
@@ -1,18 +1,5 @@
 # replace greeting
 function fish_greeting
-echo '   ___======____=---=)
- /T            \_--===)
- L \ (@)   \~    \_-==)
-  \      / )J~~    \-=)
-   \\___/  )JJ~~    \)
-    \_____/JJJ~~      \
-    / \  , \J~~~~      \
-   (-\)\=|  \~~~        L__
-   (\\)  ( -\)_            ==__
-    \V    \-\) ===_____  J\   \\
-           \V)     \_) \   JJ J\)
-                       /J JT\JJJJ)
-                       (JJJ| \UUU)
-                        (UU)'
+       neofetch
 end
 
